c36a1cb18bcf374162dd3989feed00df7452572b
Yin <yuan_yin@outlook.com>
2018-06-12 16:36:59 +0800

    Split sentence using split signs my implementation

---
+++ L16_splitSentenceWithSigns.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 11 14:50:20 2018
+
+@author: yyuan1
+"""
+
+def split_string(source,splitlist):
+    '''Type(source) = string; Type(splitlist) = string, len(splitlist) >= 1'''
+    source = [source]
+    for item in splitlist:
+        source = splitSentencesInListUsingSingleSpliter(source, item)
+    return source
+        
+def split(individule_source, individule_split):
+    '''Break a sentence(string) using single split sign (len(indiv_split) = 1)'''
+    result = []
+    while True:
+        endpos = individule_source.find(individule_split)
+        if endpos == -1:
+            result.append(individule_source[:])
+            break
+        result.append(individule_source[: endpos])
+        individule_source = individule_source[endpos + 1:]
+    return result
+
+def splitSentencesInListUsingSingleSpliter(sentenceList, individule_split):
+    '''Take in a list of sentence(s) and single split sign'''
+    result = []
+    for sentence in sentenceList:
+        result += split(sentence, individule_split)
+    return result #A list of sentence free from particular individule_split
+
+
+out = split_string("This is a test-of the,string separation-code!",",- ")
+print(out)
